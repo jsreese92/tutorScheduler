725554c0b7f68738802cfdc95d24cbc5ab3daf1c
Jordan Reese <jsreese92@gmail.com>
2013-09-27 22:53:40 -0400

    create db, create table, load sample data

---
+++ jsr/test.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <body>
+     <h1>First PHP Page</h1>
+
+     <?php
+        $x=5;
+        $y=10;
+        echo "Hello World!<br>";
+        echo "\r\n";
+        //here's a comment
+        /*here's another*/
+
+        $color = "black";
+        echo "My car is " . $color ."<br>";
+
+        function myTest(){
+          global $x, $y;
+          $y = $x + $y;
+        }
+
+        myTest();
+        echo $y;
+     ?>
+  </body>
+</html>
