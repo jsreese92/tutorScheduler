a30eb21d1f6cf432dad960390a11a0f4d83618d0
Jordan Reese <jsreese92@gmail.com>
2013-11-19 20:57:00 -0500

    part one done

--- jsr/algorithm.php
+++ jsr/algorithm.php
@@ -8,6 +8,41 @@ if (mysqli_connect_errno($con)){
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
+// global variables
+$sasbSchedule = initializeTupleArray();
+$pidArray = getPids($con);
+$preferences = initializeTupleArray();
+$days = array(sun,mon,tue,wed,thu,fri,sat);
+$hours = array(h00,h01,h02,h03,h04,h05,h06,h07,h08,h09,h10,h11,h12,h13,h14,h15,h16,h17,h18,h19,h20,h21,h22,h23);
+$availablePrefs = array(1,2,3);
+$hoursWorking = array();
+$openHours = getOpenHours($con);
+
+// array where key is PID and value is number of hours currently scheduled
+// TODO factor out into function
+foreach($pidArray as $thePid){
+  $hoursWorking[$thePid] = 0; // initialized to 0
+}
+
+function getOpenHours($theCon){
+  global $days;
+  global $hours;
+  $arr = array();
+  foreach($days as $theDay){
+    $sql = ("select * from openHours
+      where (openHours.day = '$theDay')");
+    $result = mysqli_query($theCon,$sql);
+    if($result != NULL){
+      $row = mysqli_fetch_array($result, MYSQLI_BOTH); 
+      foreach($hours as $theHour){
+        $arr[$theDay][$theHour]=$row[$theHour];
+      }
+    }
+  }
+  return $arr;
+}
+
+
 // given a day of the week, initializes its preference array
 function initializeDay($theDay){
   // array that's name is the day of the week
@@ -132,11 +167,6 @@ class tuple {
   }
 }
 
-$preferences = initializeTupleArray();
-$days = array(sun,mon,tue,wed,thu,fri,sat);
-$hours = array(h00,h01,h02,h03,h04,h05,h06,h07,h08,h09,h10,h11,h12,h13,h14,h15,h16,h17,h18,h19,h20,h21,h22,h23);
-$availablePrefs = array(1,2,3);
-
 /*
 $sql =("select hoursByDay.*, employeeInfo.type from hoursByDay, employeeInfo where hoursByDay.PID = employeeInfo.PID");
 $result = mysqli_query($con,$sql);
@@ -144,6 +174,7 @@ $numRows = mysqli_num_rows($result);
 */
 
 // populate array of tuples via a wildly ineffecient querying scheme
+// TODO factor out into function, keep in mind global vars
 foreach($days as $theDay){
   foreach($hours as $theHour){
     $sql = ("select hoursByDay.PID,employeeInfo.type,hoursByDay.$theHour
@@ -167,19 +198,6 @@ foreach($days as $theDay){
     }
   }
 }
-echo"preferences: <br>\n";
-var_dump($preferences);
-
-// 1. SASB covered for all open hours
-$sasbSchedule = initializeTupleArray();
-$pidArray = getPids($con);
-
-// array where key is PID and value is number of hours currently scheduled
-// TODO factor out into function
-$hoursWorking = array();
-foreach($pidArray as $thePid){
-  $hoursWorking[$thePid] = 0; // initialized to 0
-}
 
 function getPids($theCon){
   $arr = array();
@@ -197,84 +215,82 @@ function getPids($theCon){
 function removeTuple(&$theArray, $theDay, $theHour, $thePid){
  unset($theArray[$theDay][$theHour]["tuples"][$thePid]);
 }
-echo"removed 711582983: <br>\n";
-removeTuple($preferences, "sun", "h16", "711582983");
-var_dump($preferences);
 
-// min and max specify the minimum and maximum number of employees working
-function populateHour($theDay, $theHour, $min, $max){
+function addTuple(&$theArray, $theDay, $theHour, $thePid, $thePref, $theType){
+  global $hoursWorking;
+  $temp= new tuple;
+  $temp->setPid($thePid);
+  $temp->setTheType($theType);
+  $temp->setPref($thePref);
+
+  $theArray[$theDay][$theHour]["tuples"][$thePid]=$temp;
+  $hoursWorking[$thePid] = $hoursWorking[$thePid] + 1;
 }
 
-function populateTuples($thePref, $theType){
+function numTuples(&$theArray, $theDay, $theHour){
+  return sizeof($theArray[$theDay][$theHour]["tuples"]);
 }
 
+// 1. SASB covered for all open hours
 
-/* testing, delete
-$tuples=array();
-$test = new tuple;
-$test->setPid("test");
-$test->setTheType("test2");
-$test->setPref("test3");
-$tuples[]=$test;
+//TODO factor this junk out
+//TODO min and max matrix for hours working
+// populate sasb with grad students with 3 preference
+foreach($days as $theDay){
+  foreach($hours as $theHour){
+    if($openHours[$theDay][$theHour] == 1){
+      foreach($pidArray as $thePid){
+        $temp = new tuple;
+        $temp = $preferences[$theDay][$theHour]["tuples"][$thePid];
+        if($temp != NULL){
+          $thePref = $temp->getPref();
+          $theType = $temp->getTheType();
+          if($thePref == 3){
+            addTuple($sasbSchedule, $theDay, $theHour, $thePid, $thePref, $theType);
+          }
+        }
+      }
+    }
+  }
+}
 
-$preferences = array(
-  "sun" => array(
-    "h00" => array(
-      "tuples" => array(
-      )
-    ),
-    "h01" => array(
-    ),
-    "h02" => array(
-    ),
-    "h03" => array(
-    ),
-    "h04" => array(
-    ),
-    "h05" => array(
-    ),
-    "h06" => array(
-    ),
-    "h07" => array(
-    ),
-    "h08" => array(
-    ),
-    "h09" => array(
-    ),
-    "h10" => array(
-    ),
-    "h11" => array(
-    ),
-    "h12" => array(
-    ),
-    "h12" => array(
-    ),
-    "h13" => array(
-    ),
-    "h14" => array(
-    ),
-    "h15" => array(
-    ),
-    "h16" => array(
-    ),
-    "h17" => array(
-    ),
-    "h18" => array(
-    ),
-    "h19" => array(
-    ),
-    "h20" => array(
-    ),
-    "h21" => array(
-    ),
-    "h22" => array(
-    ),
-    "h23" => array(
-    )
-  )
-);
+// check which hours have less than two, and add 2s as necessary
+foreach($days as $theDay){
+  foreach($hours as $theHour){
+    if(($openHours[$theDay][$theHour] == 1) and (numTuples($sasbSchedule, $theDay, $theHour) < 2)){
+      foreach($pidArray as $thePid){
+        $temp = new tuple;
+        $temp = $preferences[$theDay][$theHour]["tuples"][$thePid];
+        if($temp != NULL){
+          $thePref = $temp->getPref();
+          $theType = $temp->getTheType();
+          if($thePref == 2){
+            addTuple($sasbSchedule, $theDay, $theHour, $thePid, $thePref, $theType);
+          }
+        }
+      }
+    }
+  }
+}
 
-$preferences["sun"]["h00"]["tuples"] = $tuples;
-//var_dump($preferences["sun"]["h00"]["tuples"]);
- */
+// same thing with 1s
+foreach($days as $theDay){
+  foreach($hours as $theHour){
+    if(($openHours[$theDay][$theHour] == 1) and (numTuples($sasbSchedule, $theDay, $theHour) < 2)){
+      foreach($pidArray as $thePid){
+        $temp = new tuple;
+        $temp = $preferences[$theDay][$theHour]["tuples"][$thePid];
+        if($temp != NULL){
+          $thePref = $temp->getPref();
+          $theType = $temp->getTheType();
+          if($thePref == 1){
+            addTuple($sasbSchedule, $theDay, $theHour, $thePid, $thePref, $theType);
+          }
+        }
+      }
+    }
+  }
+}
+echo"sasbSchedule\n";
+var_dump($sasbSchedule);
 ?>
