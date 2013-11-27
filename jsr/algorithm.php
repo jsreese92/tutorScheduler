def7a71575d5017c2bb2d577fabf59866d798dd9
Jordan Reese <jsreese92@gmail.com>
2013-11-27 11:37:40 -0500

    factored out functions

--- jsr/algorithm.php
+++ jsr/algorithm.php
@@ -10,28 +10,33 @@ if (mysqli_connect_errno($con)){
 
 // global variables
 $sasbSchedule = initializeTupleArray();
-$pidArray = getPids($con);
+$pidArray = getPids();
 $preferences = initializeTupleArray();
 $days = array(sun,mon,tue,wed,thu,fri,sat);
 $hours = array(h00,h01,h02,h03,h04,h05,h06,h07,h08,h09,h10,h11,h12,h13,h14,h15,h16,h17,h18,h19,h20,h21,h22,h23);
-$availablePrefs = array(1,2,3);
-$hoursWorking = array();
-$openHours = getOpenHours($con);
+$hoursWorking = initializeHoursWorking();
+$openHours = getOpenHours();
 
 // array where key is PID and value is number of hours currently scheduled
-// TODO factor out into function
-foreach($pidArray as $thePid){
-  $hoursWorking[$thePid] = 0; // initialized to 0
+function initializeHoursWorking(){
+  global $pidArray;
+  $arr = array();
+  foreach($pidArray as $thePid){
+    $arr[$thePid] = 0; // initialized to 0
+  }
+  return $arr;
 }
 
-function getOpenHours($theCon){
+// returns array of hours that are available to be scheduled
+function getOpenHours(){
+  global $con;
   global $days;
   global $hours;
   $arr = array();
   foreach($days as $theDay){
     $sql = ("select * from openHours
       where (openHours.day = '$theDay')");
-    $result = mysqli_query($theCon,$sql);
+    $result = mysqli_query($con,$sql);
     if($result != NULL){
       $row = mysqli_fetch_array($result, MYSQLI_BOTH); 
       foreach($hours as $theHour){
@@ -174,35 +179,42 @@ $numRows = mysqli_num_rows($result);
 */
 
 // populate array of tuples via a wildly ineffecient querying scheme
-// TODO factor out into function, keep in mind global vars
-foreach($days as $theDay){
-  foreach($hours as $theHour){
-    $sql = ("select hoursByDay.PID,employeeInfo.type,hoursByDay.$theHour
-      from hoursByDay, employeeInfo
-      where (hoursByDay.day = '$theDay' and $theHour > 0 and 
-      hoursByDay.PID = employeeInfo.PID)");
+function loadPref(){ // returns preferences tuple array
+  global $con;
+  global $days;
+  global $hours;
+  global $preferences;
+  foreach($days as $theDay){
+    foreach($hours as $theHour){
+      $sql = ("select hoursByDay.PID,employeeInfo.type,hoursByDay.$theHour
+        from hoursByDay, employeeInfo
+        where (hoursByDay.day = '$theDay' and $theHour > 0 and 
+        hoursByDay.PID = employeeInfo.PID)");
 
-    $result = mysqli_query($con,$sql);
-    $numRows = mysqli_num_rows($result);
+      $result = mysqli_query($con,$sql);
+      $numRows = mysqli_num_rows($result);
 
-    for($i = 0; $i < $numRows; $i++){ // for every row
-      // get the array from result indexed by both numbers and variable name
-      $row = mysqli_fetch_array($result, MYSQLI_BOTH); 
-      //var_dump($row); 
-      $temp= new tuple;
-      $temp->setPid($row["PID"]);
-      $temp->setTheType($row["type"]);
-      $temp->setPref("$row[$theHour]");
-      // associative array where an employee's PID points to his/her tuple
-      $preferences[$theDay][$theHour]["tuples"][$row["PID"]]=$temp;
+      for($i = 0; $i < $numRows; $i++){ // for every row
+        // get the array from result indexed by both numbers and variable name
+        $row = mysqli_fetch_array($result, MYSQLI_BOTH); 
+        //var_dump($row); 
+        $temp= new tuple;
+        $temp->setPid($row["PID"]);
+        $temp->setTheType($row["type"]);
+        $temp->setPref("$row[$theHour]");
+        // associative array where an employee's PID points to his/her tuple
+        $preferences[$theDay][$theHour]["tuples"][$row["PID"]]=$temp;
+      }
     }
   }
 }
 
-function getPids($theCon){
+
+function getPids(){
+  global $con;
   $arr = array();
   $sql = ("select PID from employeeInfo");
-  $result = mysqli_query($theCon,$sql);
+  $result = mysqli_query($con,$sql);
   $numRows = mysqli_num_rows($result);
   for($i = 0; $i<$numRows; $i++){
     $row = mysqli_fetch_array($result, MYSQLI_BOTH);
@@ -231,10 +243,45 @@ function numTuples(&$theArray, $theDay, $theHour){
   return sizeof($theArray[$theDay][$theHour]["tuples"]);
 }
 
-// 1. SASB covered for all open hours
+// loops through preferences array starting with highest preferences, and adds
+// tuples to the given schedule until at least two people are working every
+// hour.
+function ensureTwoScheduled(&$theSchedule){
+  global $days;
+  global $hours;
+  global $openHours;
+  global $pidArray;
+  global $preferences;
+  for($i = 3; $i >= 1; $i--){
+    foreach($days as $theDay){
+      foreach($hours as $theHour){
+        if($openHours[$theDay][$theHour] == 1){
+          foreach($pidArray as $thePid){
+            $temp = new tuple;
+            $temp = $preferences[$theDay][$theHour]["tuples"][$thePid];
+            if($temp != NULL){
+              $thePref = $temp->getPref();
+              $theType = $temp->getTheType();
+              if($thePref == $i){
+                if(numTuples($theSchedule, $theDay, $theHour) < 2){
+                  addTuple($theSchedule, $theDay, $theHour, $thePid, $thePref, $theType);
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
 
-//TODO factor this junk out
+// 1. SASB covered for all open hours
 //TODO min and max matrix for hours working
+loadPref();
+ensureTwoScheduled($sasbSchedule);
+
+
+/*
 // populate sasb with grad students with 3 preference
 foreach($days as $theDay){
   foreach($hours as $theHour){
@@ -291,6 +338,9 @@ foreach($days as $theDay){
     }
   }
 }
+ */
 echo"sasbSchedule\n";
+echo"<pre>";
 var_dump($sasbSchedule);
+echo"</pre>";
 ?>
